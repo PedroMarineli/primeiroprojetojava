f3c66ab6aef7c79f4b532d9a64c83001bcbf81f6
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2024-02-29 15:31:26 -0300

    first commit

---
+++ src/Estudos.java
@@ -0,0 +1,9 @@
+public class Estudos{
+    
+    public static void main(String[] args){
+        System.out.println("JVM (Java Virtual Machine)");
+        System.out.println("JRE (Java Runtime Environment)");
+    }
+    
+}
+
